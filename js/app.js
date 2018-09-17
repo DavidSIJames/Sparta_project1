66b9e61d9e764bd39d396cff96a84e6ce910bcc0
Tech-A46 <tech-a46@Tech-a46s-MacBook-Air.local>
2018-09-17 11:15:27 +0100

    completed the game start screen

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded",function(){
-  console.log("working");
+  const game = new Object ();
+    game.screen = document.getElementById('gameScreen');
+    game.gameStart = () => {
+      game.startTitle = document.createElement("h1");
+      game.textAdder = document.createTextNode("Speed Typer");
+      game.startTitle.classList.add("gameText");
+      game.startTitle.appendChild(game.textAdder);
+      game.screen.appendChild(game.startTitle);
+      game.startTitle.style.marginTop= "200px";
+      game.startTitle.style.paddingBottom = "60px";
+      game.startBtn = document.createElement("h2");
+      game.startBtn.classList.add("gameText");
+      game.textAdder = document.createTextNode("start");
+      game.startBtn.appendChild(game.textAdder);
+      game.screen.appendChild(game.startBtn);
+
+      game.startBtn.addEventListener('mouseenter', function(){
+        game.startBtn.style.color = "#c22929";
+      });
+      game.startTitle.addEventListener('mouseleave',function(){
+        game.startBtn.style.color = "#0fd53b";
+      });
+      game.startBtn.addEventListener('click',function(){
+        alert("start button clicked!");
+      });
+    };
+    game.gameStart();
 });
